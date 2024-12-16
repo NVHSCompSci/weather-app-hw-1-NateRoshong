63f908f3e97129d3530ac78e2641eb0a309b54b0
NateRoshong <142993058+NateRoshong@users.noreply.github.com>
2024-12-16 13:52:28 -0500

    js update

--- .github/app.js
+++ .github/app.js
@@ -3,10 +3,18 @@ function updateWeatherInfo(response) {
     // pulls weather data or city searched
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
-    let descriptionElement = document.querySelector('#description')
+    let cityElement = document.querySelector("#city")
+    cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML = Math.round(temperature);
-    descriptionElement.innerHTML = response.data.condition.description;
 }
+
+
+function searchCity(city){
+    let apiKey = "a3o950fc274379347b6a44aft08a3cb0";
+    let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiURL).then(updateWeatherInfo);
+}
+
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
@@ -17,10 +25,5 @@ function handleSearchSubmit(event){
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-
-function searchCity(city){
-    let apiKey = "a3o950fc274379347b6a44aft08a3cb0";
-    let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-    axios.get(apiURL).then(updateWeatherInfo);
-}
 searchCity("London");
+console.log(response.data.condition.description);
